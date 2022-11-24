705f8c42aff22ea2df0c884d19b09a97cd62a168
FedeSaffores <safforesfederico@gmail.com>
2022-11-24 17:58:11 -0300

    confirguro la ruta serviceController

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,7 @@
-const { Router } = require('express');
+const { Router, json } = require("express");
 const router = Router();
+router.use(json());
+const serviceController = require("../middlewares/service");
 
+router.use("/service", serviceController);
 module.exports = router;
