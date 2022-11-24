60953bccff3ce77e00f28e1120d6250aabb4b8f8
FedeSaffores <safforesfederico@gmail.com>
2022-11-24 17:06:39 -0300

    Cambios en Admin y Service Integer createdAt y updatedAt a false

--- api/src/models/Admin.js
+++ api/src/models/Admin.js
@@ -1,59 +1,63 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-    sequelize.define("Admin", {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true,
-            allowNull: false
+  sequelize.define(
+    "Admin",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [1, 50],
+            msg: "The name should contain at least 1 letter",
+          },
         },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: {
-                    args: [1, 50],
-                    msg: "The name should contain at least 1 letter",
-                }
-            }
+      },
+      lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [1, 50],
+            msg: "The lastname should contain at least 1 letter",
+          },
         },
-        lastname: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: {
-                    args: [1, 50],
-                    msg: "The lastname should contain at least 1 letter",
-                }
-            }
+      },
+      adminame: {
+        type: DataTypes.TEXT,
+        unique: true,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [1, 50],
+            msg: "Username should have 8 characters at least",
+          },
+          is: /^[0-9a-f]{64}$/i,
         },
-        username: {
-            type: DataTypes.TEXT,
-            unique: true,
-            allowNull: false,
-            validate: {
-                len: {
-                    args: [1, 50],
-                    msg: "Username should have 8 characters at least"
-                },
-                is: /^[0-9a-f]{64}$/i
-            }
+      },
+      password: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [8, 24],
+            msg: "Password should have 8 characters at least",
+          },
+          is: /^[0-9a-f]{64}$/i,
         },
-        password: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-            validate: {
-                len: {
-                    args: [8, 24],
-                    msg: "Password should have 8 characters at least"
-                },
-                is: /^[0-9a-f]{64}$/i
-            }
-        }
-    }, {
-        timestamp: false,
-        createdAt: false,
-        updatedAt: false,
-    })
+      },
+    },
+    {
+      timestamp: false,
+      createdAt: false,
+      updatedAt: false,
+    }
+  );
 };
--- api/src/models/Service.js
+++ api/src/models/Service.js
@@ -1,53 +1,71 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-    sequelize.define("Service", {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true,
-            allowNull: false
+  sequelize.define(
+    "Service",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      type: {
+        type: DataTypes.ENUM(["Health", "Aesthetics"]),
+        defaultValue: "Health",
+        validate: {
+          isIn: {
+            args: [["Health", "Aesthetics"]],
+            msg: "It should be Health or Aesthetics",
+          },
         },
-        type: {
-            type: DataTypes.ENUM("Health", "Aesthetics"),
-            defaultValue: "Health",
-            validate: {
-                isIn: {
-                    args: ["Health", "Aesthetics"],
-                    msg: "It should be Health or Aesthetics",
-                }
-            }
+      },
+      name: {
+        type: DataTypes.STRING,
+        defaultValue: false,
+        validate: {
+          len: {
+            args: [1, 50],
+            msg: "The name should contain at least 1 letter",
+          },
         },
-        price: {
-            type: DataTypes.NUMBER,
-            defaultValue: 0,
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      timelapse: {
+        type: DataTypes.INTEGER,
+        defaultValue: 60,
+        validate: {
+          min: 60,
+          max: 2880,
         },
-        timelapse: {
-            type: DataTypes.INTEGER,
-            defaultValue: 10,
-            validate: {
-                min: 60,
-                max: 2880
-            }
+      },
+      inicialDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      finishDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      review: {
+        type: DataTypes.TEXT,
+      },
+      average: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+        validate: {
+          min: 0,
+          max: 5,
         },
-        date: {
-            type: DataTypes.DATE,
-            allowNull: false
-        },
-        review: {
-            type: DataTypes.TEXT
-        },
-        average: {
-            type: DataTypes.NUMBER,
-            defaultValue: 0,
-            validate: {
-                min: 0,
-                max: 5
-            }
-        }
-    }, {
-        timestamps: false,
-        createdAt: null,
-        updatedAt: null
-    })
+      },
+    },
+    {
+      timestamps: false,
+      createdAt: false,
+      updatedAt: false,
+    }
+  );
 };
