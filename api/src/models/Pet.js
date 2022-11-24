71248153cad2f4acbbb45a3d040cf01c4aa252e8
Gaston Valles <gastonvallesyeou@gmail.com>
2022-11-23 22:01:27 -0300

    Creation of User and Pet models

---
+++ api/src/models/Pet.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define("Pet", {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: {
+                    args: [1, 50],
+                    msg: "The name should contain at least 1 letter",
+                }
+            }
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        detail: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }, {
+        timestamps: false,
+        createdAt: null,
+        updatedAt: null,
+    })
+};
---
+++ api/src/models/User.js
@@ -0,0 +1,86 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define("User", {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: {
+                    args: [1, 50],
+                    msg: "The name should contain at least 1 letter",
+                }
+            }
+        },
+        lastname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: {
+                    args: [1, 50],
+                    msg: "The lastname should contain at least 1 letter",
+                }
+            }
+        },
+        username: {
+            type: DataTypes.TEXT,
+            unique: true,
+            allowNull: false,
+            validate: {
+                len: {
+                    args: [1, 50],
+                    msg: "Username should have 8 characters at least"
+                },
+                is: /^[0-9a-f]{64}$/i
+            }
+        },
+        password: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: {
+                    args: [8, 24],
+                    msg: "Password should have 8 characters at least"
+                },
+                is: /^[0-9a-f]{64}$/i
+            }
+        },
+        email: {
+            type: DataTypes.TEXT,
+            unique: true,
+            allowNull: false,
+            validate: {
+                is: /^[0-9a-f]{64}$/i
+            }
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        dni: {
+            type: DataTypes.INTEGER,
+            unique: true,
+            allowNull: false
+        },
+        isAdmin: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+            allowNull: false
+        },
+        isActive: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true,
+            allowNull: false
+        }
+    }, {
+        timestamps: false,
+        createdAt: null,
+        updatedAt: null
+    })
+};
