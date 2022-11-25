3a476a4f5a97af68247d4bf61e23f299ec4834cb
Luis Fernando Goytia Cabezas <luis.goytia.IT@gmail.com>
2022-11-25 14:05:35 -0300

    add formUser

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
-import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from "react-router-dom";
+import FormUser from './Components/Funcionality/Forms/User';
 
 function App() {
   return (
     <div className="App">
-      <Routes></Routes>
+      <Routes>
+        <Route path='/' element={<FormUser/>} />
+      </Routes>
     </div>
   );
 }
