5b42d92296850d9169efd63205046d19d1cf7a9d
FedeSaffores <safforesfederico@gmail.com>
2022-11-24 22:38:44 -0300

    creo las rutas, middlewares y controllers del admin comento router post

--- api/src/middlewares/Admin.js
+++ api/src/middlewares/Admin.js
@@ -34,7 +34,8 @@ router.get("/", async (req, res) => {
     res.status(404).send(error.message);
   }
 });
-router.post("/", async (req, res) => {
+{
+  /* router.post("/", async (req, res) => {
   try {
     console.log(req.body);
     const createAdmin = await dbCreate(req.body);
@@ -44,5 +45,6 @@ router.post("/", async (req, res) => {
     res.status(404).send(error.message);
   }
 });
-
+*/
+}
 module.exports = router;
