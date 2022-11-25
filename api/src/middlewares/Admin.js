5ffc34bd34006c6548117124e6cf4c7ab81c4a52
FedeSaffores <safforesfederico@gmail.com>
2022-11-24 22:27:19 -0300

    creo las rutas, middlewares y controllers del admin

---
+++ api/src/middlewares/Admin.js
@@ -0,0 +1,48 @@
+const { Router } = require("express");
+const {
+  getDBAdmin,
+  getDBAdminByPK,
+  getAdminByName,
+  dbCreate,
+} = require("../controllers/getAllAdmin");
+
+const router = Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const allAdmin = await getDBAdmin(req.query.name);
+    console.log(allAdmin);
+    res.status(200).json(allAdmin);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const adminPk = await getDBAdminByPK(id);
+    res.status(200).json(adminPk);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+router.get("/", async (req, res) => {
+  try {
+    const allAdmin = await getAdminByName(req.query.name);
+    res.status(200).json(allAdmin);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+router.post("/", async (req, res) => {
+  try {
+    console.log(req.body);
+    const createAdmin = await dbCreate(req.body);
+
+    res.status(200).json(createAdmin);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+module.exports = router;
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,6 +2,9 @@ const { Router, json } = require("express");
 const router = Router();
 router.use(json());
 const serviceController = require("../middlewares/service");
+const admController = require("../middlewares/Admin");
 
+router.use("/admin", admController);
 router.use("/service", serviceController);
+
 module.exports = router;
