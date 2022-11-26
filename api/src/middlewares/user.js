2364fb3eb9ebb593425f3075fe6683f717d18f59
Gaston Valles <gastonvallesyeou@gmail.com>
2022-11-26 20:36:17 -0300

    fix getallusers

--- api/src/middlewares/user.js
+++ api/src/middlewares/user.js
@@ -1,18 +1,39 @@
 const { Router } = require("express");
 const {
-    dbCreateUser
+  dbCreateUser
 } = require("../controllers/postUser");
-
+const {
+  getAllUsers,
+  getUserByPK
+} = require("../controllers/getAllUsers");
 const router = Router();
 
-router.post("/", async (req, res) => {
-    try {
-      console.log(req.body);
-      const createUser = await dbCreateUser(req.body);
-      res.status(200).json(createUser);
-    } catch (error) {
-      res.status(404).send(error.message);
-    }
-  });
+router.get('/', async (req, res) => {
+  try {
+    const user = await getAllUsers(req.query.name);
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
 
-  module.exports = router;
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userId = await getUserByPK(id);
+    res.status(200).send(userId);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const createUser = await dbCreateUser(req.body);
+    res.status(200).json(createUser);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+module.exports = router;
