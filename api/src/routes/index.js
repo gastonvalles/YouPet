f3fa662578351f9b0763e56f488ef0987b2aee06
FedeSaffores <safforesfederico@gmail.com>
2022-11-26 19:14:47 -0300

    creo ccontrollers, middlewares y rutas de vet

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,12 +5,13 @@ const admController = require("../middlewares/admin");
 
 const userController = require("../middlewares/user");
 const petController = require("../middlewares/pet");
-
+const vetController = require("../middlewares/vet");
 router.use(json());
 
 router.use("/admin", admController);
 router.use("/service", serviceController);
 router.use("/user", userController);
 router.use("/pet", petController);
+router.use("/vet", vetController);
 
 module.exports = router;
