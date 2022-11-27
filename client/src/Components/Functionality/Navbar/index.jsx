7213ad5299e3ecdd2471cafad5ab94e977bd4b68
Luis Fernando Goytia Cabezas <luis.goytia.IT@gmail.com>
2022-11-27 16:42:03 -0300

    add button login

--- client/src/Components/Functionality/Navbar/index.jsx
+++ client/src/Components/Functionality/Navbar/index.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../../../img/logo.png"
+import logo from "../../../img/logo.png";
 
 export default function Navbar() {
   return (
     <>
       <nav className="navbar navbar-expand-md bg-info">
-        <div className="container-fluid" >
+        <div className="container-fluid">
           <Link className="Link  me-4" to={"/Home"}>
-            <img
-              src={logo}
-              alt="Bootstrap"
-              height="55"
-            />
+            <img src={logo} alt="Bootstrap" height="55" />
           </Link>
           <button
             className="navbar-toggler"
@@ -28,14 +24,14 @@ export default function Navbar() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-1">
               <li className="nav-item">
-                <Link className="Link" to={"/Home"}>
+                <Link to={"/Home"}>
                   <span className="nav-link active me-3" aria-current="page">
                     Home
                   </span>
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="Link" to={"/Home"}>
+                <Link to={"/Home"}>
                   <span className="nav-link active me-3" aria-current="page">
                     Contac
                   </span>
@@ -52,29 +48,25 @@ export default function Navbar() {
                 </span>
                 <ul className="dropdown-menu">
                   <li>
-                    <Link className="Link" to={"/veterinario"}>
-                      <span className="dropdown-item" />
-                      1 Option
+                    <Link to={"/veterinario"}>
+                      <span className="dropdown-item" />1 Option
                     </Link>
                   </li>
                   <li>
-                    <Link className="Link" to={"/nutricionista"}>
-                      <span className="dropdown-item" />
-                      2 Option
+                    <Link to={"/nutricionista"}>
+                      <span className="dropdown-item" />2 Option
                     </Link>
                   </li>
+                  <li></li>
                   <li>
-                  </li>
-                  <li>
-                    <Link className="Link" to={"/cirugias"}>
-                      <span className="dropdown-item" />
-                      3 Option
+                    <Link to={"/cirugias"}>
+                      <span className="dropdown-item" />3 Option
                     </Link>
                   </li>
                 </ul>
               </li>
               <li className="nav-item">
-                <a className="nav-link">Disabled</a>
+                <span className="nav-link">Disabled</span>
               </li>
             </ul>
             <form className="d-flex" role="search">
@@ -84,10 +76,21 @@ export default function Navbar() {
                 placeholder="Search"
                 aria-label="Search"
               />
-              <button className="btn btn-outline-success" type="submit">
+              <button className="btn btn-outline-success me-4" type="submit">
                 Search
               </button>
             </form>
+            {/* preguntar si esta logueado o no */}
+            <div className="d-flex">
+              <Link to={"/login"}>
+                <button className="btn"> Sign in</button>
+              </Link>
+              <Link to={"/reguser"}>
+                <button className="btn btn-outline-success me-4">
+                  Sign up
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </nav>
