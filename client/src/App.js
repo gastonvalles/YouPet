41f69ba249490b60fadc5044186bfd1705311212
Luis Fernando Goytia Cabezas <luis.goytia.IT@gmail.com>
2022-11-27 15:52:23 -0300

    add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-
-
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import { Routes, Route } from "react-router-dom";
 import Home from "./Components/View/HomeFake";
 import VetDetail from "./Components/Functionality/Details/VetDetail";
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route } from "react-router-dom";
-import FormHistoryPet from './Components/Funcionality/Forms/HistoryPet';
-import FormUser from './Components/Funcionality/Forms/User';
-
+import FormHistoryPet from "./Components/Functionality/Forms/HistoryPet";
+import FormUser from "./Components/Functionality/Forms/User";
+import Navbar from "./Components/Functionality/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-
         <Route path="/" element={<Home />} />
         <Route path="/:id" element={<VetDetail />} />
-
-        <Route exact path='/reguser' element={<FormUser />} />
-        <Route exact path='/formdescrip' element={<FormHistoryPet/>}/>
-
+        <Route exact path="/reguser" element={<FormUser />} />
+        <Route exact path="/formdescrip" element={<FormHistoryPet />} />
+        <Route exact path="/navbar" element ={<Navbar/>}/>
       </Routes>
     </div>
   );
