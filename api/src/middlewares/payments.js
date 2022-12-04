7ced19a26e543385ceb550fd003426c3f40ffe3e
Luis Fernando Goytia Cabezas <luis.goytia.IT@gmail.com>
2022-12-03 22:31:02 -0300

    Traer estados de merchant_order

--- api/src/middlewares/payments.js
+++ api/src/middlewares/payments.js
@@ -1,10 +1,11 @@
-const { Router } = require("express");
+const { Router, query } = require("express");
 const {getPaymentLink} = require ("../controllers/getPaymentLink")
 require("dotenv").config();
 const { ACCESS_TOKEN } = process.env;
 
 // SDK de Mercado Pago
 const mercadopago = require("mercadopago");
+const { merchant_orders } = require("mercadopago");
 // Agrega credenciales
 mercadopago.configure({
   access_token: ACCESS_TOKEN,
@@ -30,10 +31,28 @@ router.post("/mp", async (req, res) => {
     .catch((error) => console.log(error));
 });
 
-router.post('/notify', (req, res) => {
+router.post('/notify', async (req, res) => {
   const { body, query } = req;
   console.log("notificar")
-  console.log({ body, query });
+  console.log({ query });
+  //mercadopago.merchant_orders.findById(id).then(res=>console.log(res.body))
+  let topic = query.topic || query.type
+  switch (topic) {
+    case "payment":
+      const paymentId = query.id || query['data.id'];
+      console.log(topic, 'getting payment', paymentId);
+      let payment = await mercadopago.payment.findById(paymentId);
+      //console.log(payment);
+      console.log(topic, 'getting merchant order');
+      let merchantOrder = await mercadopago.merchant_orders.findById(payment.body.order.id)
+      break;
+    
+    case "merchart_order":
+      const orderId = query.id;
+      console.log(topic, 'getting merchan order', orderId);
+      merchantOrder = await mercadopago.merchant_orders.findById(orderId)
+      break;
+  }
 })
 
 module.exports = router;
