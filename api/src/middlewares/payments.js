17f1815700d7875b4c7976064d848cefc2283048
Luis Fernando Goytia Cabezas <luis.goytia.IT@gmail.com>
2022-12-04 12:42:53 -0300

    estado mercado pago

--- api/src/middlewares/payments.js
+++ api/src/middlewares/payments.js
@@ -1,5 +1,5 @@
 const { Router, query } = require("express");
-const {getPaymentLink} = require ("../controllers/getPaymentLink")
+const { getPaymentLink } = require("../controllers/getPaymentLink");
 require("dotenv").config();
 const { ACCESS_TOKEN } = process.env;
 
@@ -31,28 +31,43 @@ router.post("/mp", async (req, res) => {
     .catch((error) => console.log(error));
 });
 
-router.post('/notify', async (req, res) => {
-  const { body, query } = req;
-  console.log("notificar")
+router.post("/notify", async (req, res) => {
+  const { query } = req;
+  console.log("notificar");
   console.log({ query });
-  //mercadopago.merchant_orders.findById(id).then(res=>console.log(res.body))
-  let topic = query.topic || query.type
+  //Ya que llegan dos tipos de objetos con los datos desde el query (asi es el sist de mercado pago)
+  //hago un swicht para determinar de que tipo es para poder  trabajarlo
+  let topic = query.topic || query.type;
   switch (topic) {
     case "payment":
-      const paymentId = query.id || query['data.id'];
-      console.log(topic, 'getting payment', paymentId);
+      const paymentId = query.id || query["data.id"];
+      console.log(topic, "getting payment", paymentId);
       let payment = await mercadopago.payment.findById(paymentId);
       //console.log(payment);
-      console.log(topic, 'getting merchant order');
-      let merchantOrder = await mercadopago.merchant_orders.findById(payment.body.order.id)
+      console.log(topic, "getting merchant order");
+      var { body } = await mercadopago.merchant_orders.findById(
+        payment.body.order.id
+      );
       break;
-    
+
     case "merchart_order":
       const orderId = query.id;
-      console.log(topic, 'getting merchan order', orderId);
-      merchantOrder = await mercadopago.merchant_orders.findById(orderId)
+      console.log(topic, "getting merchan order", orderId);
+      var { body } = await mercadopago.merchant_orders.findById(orderId);
       break;
   }
-})
+  //comprobar si el estado de cada pago si se cerro y aprobo el pago
+  let paidAmount = 0;
+  body.payments.forEach((payment) => {
+    if (payment.status === "approved") {
+      paidAmount += payment.transaction_amount;
+    }
+  });
+  if (paidAmount >= body.total_amount) {
+    console.log("El pago se concreto");
+  } else {
+    console.log("El pago no se concreto");
+  }
+});
 
 module.exports = router;
