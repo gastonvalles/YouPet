881cab9e89ffead6cfdfd0680c77dfcd49413769
Luis Fernando Goytia Cabezas <luis.goytia.IT@gmail.com>
2022-12-05 01:31:50 -0300

    get Arreglado

--- api/src/controllers/getPayment.js
+++ api/src/controllers/getPayment.js
@@ -1,9 +1,8 @@
 const { Sequelize } = require("sequelize");
-const Payments = require("../db");
+const {Payments} = require("../db");
 
 const getPayment = async (idPreference) => {
-    console.log(idPreference);
-    const paidDB = await Payments.findAll({
+    const paidDB = await Payments.findOne({
         where: { idPreference:idPreference },
     });
     console.log("lo encontre ",paidDB);
