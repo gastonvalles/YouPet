f89d558982665cb42c405ad5dced85917d71d166
FedeSaffores <safforesfederico@gmail.com>
2022-12-09 10:50:26 -0300

    agrago controller favoritos

--- api/src/controllers/getAllFavoritos.js
+++ api/src/controllers/getAllFavoritos.js
@@ -1,1 +1,25 @@
-const {Favoritos} = require("../db");
+const { Service, User } = require("../db");
+
+const getAllFavorites = async (UserId, ServiceId) => {
+  const favoritos = await User.findOne({
+    where: { id },
+    include: Service,
+  });
+  if (!favoritos.length) {
+    throw new Error("no existen favoritos");
+  }
+  return favoritos;
+};
+
+const dbFavoriteCreate = async (params) => {
+  const { userId, serviceId } = params;
+  if (!userId || !serviceId) {
+    throw new Error("missing params");
+  }
+  try {
+    await create();
+    return `Service ${body.name} created successfully`;
+  } catch (error) {
+    throw error;
+  }
+};
