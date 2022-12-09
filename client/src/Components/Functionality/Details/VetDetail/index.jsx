2864f8f04cbd2d01ffdc92731cf3a083955e3df0
FedeSaffores <safforesfederico@gmail.com>
2022-12-09 11:16:11 -0300

    realizo cambios en componente vet

--- client/src/Components/Functionality/Details/VetDetail/index.jsx
+++ client/src/Components/Functionality/Details/VetDetail/index.jsx
@@ -1,21 +1,13 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { clearDetails, getVetsDetail } from "../../../../Redux/actions";
-import './vetDetail.css';
+import "./vetDetail.css";
 
 export default function VetDetail() {
   let { id } = useParams();
   const dispatch = useDispatch();
   const vet = useSelector((state) => state.vetDetail);
-  const [, navigate] = useLocation();
-
-  const addFav = (id) => {
-    //if (no esta logeado) {
-    //return navigate("/login");
-    alert(id)
-  }
-
 
   useEffect(() => {
     dispatch(getVetsDetail(id));
@@ -26,8 +18,8 @@ export default function VetDetail() {
 
   return (
     <div className="vet-cards">
-      <div className='vet-card-detail'>
-        <img className='vet-profile-photo' src={vet.img} alt="Not found" />
+      <div className="vet-card-detail">
+        <img className="vet-profile-photo" src={vet.img} alt="Not found" />
         <div>
           <h1 className="vet-profile-name">
             {vet.name} {vet.lastname}
@@ -35,10 +27,6 @@ export default function VetDetail() {
           <h2 className="vet-speciality">{vet.speciality}</h2>
         </div>
         <h3>Average: {vet.average}</h3>
-        <div>
-          <button onClick={() => { addFav(id) }}> ❤️</button>
-          <span>{vet.fav}</span>
-        </div>
       </div>
 
       <div className="vet-comments">
@@ -60,9 +48,7 @@ export default function VetDetail() {
             <h3 className="client-comment">Muy buena atencion xd</h3>
           </div>
         </div>
-
       </div>
-
     </div>
   );
 }
