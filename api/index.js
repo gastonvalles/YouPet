78d13bfac9cae54cbcabe8e7976f25c7b1af0581
Klevens <kelvinreyescnh@gmail.com>
2022-12-13 00:16:02 -0400

    force true

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
     server.listen(process.env.PORT, () => {
         console.log(`Server listening at ${process.env.PORT}`);
     });
