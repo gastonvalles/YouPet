2aeea5a26255356be0444caa8ce264fc25a0b1d9
Klevens <kelvinreyescnh@gmail.com>
2022-12-13 00:13:32 -0400

    userpanel half

--- client/src/Components/Functionality/UserPanel/Info/index.jsx
+++ client/src/Components/Functionality/UserPanel/Info/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Box } from "@mui/material";
 import Header from "../Header";
 import userPlaceholder from "../../../../img/user-placeholder.png";
@@ -12,7 +12,6 @@ export default function Info() {
   const myuser = useSelector((state) => state.myuser);
   const myturns = useSelector((state) => state.turns);
 
-  console.log(myuser);
 
   return (
     <Box m="20px">
@@ -20,265 +19,194 @@ export default function Info() {
         <Header title="Info" subtitle="Welcome" />
       </Box>
       <Box>
+        {myuser?.id ? (
+          <div className={infoStyle.container}>
+            <div className={infoStyle.card}>
+              <img
+                src={myuser.img ? myuser.img : userPlaceholder}
+                alt="User"
+                className="rounded-circle"
+                width="150"
+              />
+              <div className="mt-3">
+                <h4>
+                  {myuser.name ? myuser.name : "none"}{" "}
+                  {myuser.lastname ? myuser.lastname : ""}
+                </h4>
+                <p className="text-secondary mb-1">Youpet user</p>
+               
 
 
-
-
-        <div>
-        <div className="container">
-    <div className="main-body">
-    
-          
-          <nav aria-label="breadcrumb" className="main-breadcrumb">
-            <ol className="breadcrumb">
-              <li className="breadcrumb-item"><a href="index.html">Home</a></li>
-              <li className="breadcrumb-item"><a href="javascript:void(0)">User</a></li>
-              <li className="breadcrumb-item active" aria-current="page">User Profile</li>
-            </ol>
-          </nav>
-          
-    
-          <div className="row gutters-sm">
-            <div className="col-md-4 mb-3">
-              <div className="card">
-                <div className="card-body">
-                  <div className="d-flex flex-column align-items-center text-center">
-                    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width="150"/>
-                    <div className="mt-3">
-                      <h4>John Doe</h4>
-                      <p className="text-secondary mb-1">Full Stack Developer</p>
-                      <p className="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
-                      <button className="btn btn-primary">Follow</button>
-                      <button className="btn btn-outline-primary">Message</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="card mt-3">
-                <ul className="list-group list-group-flush">
-                  <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 className="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-globe mr-2 icon-inline"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>Website</h6>
-                    <span className="text-secondary">https://bootdey.com</span>
-                  </li>
-                  <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 className="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-github mr-2 icon-inline"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>Github</h6>
-                    <span className="text-secondary">bootdey</span>
-                  </li>
-                  <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 className="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-twitter mr-2 icon-inline text-info"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>Twitter</h6>
-                    <span className="text-secondary">@bootdey</span>
-                  </li>
-                  <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 className="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-instagram mr-2 icon-inline text-danger"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>Instagram</h6>
-                    <span className="text-secondary">bootdey</span>
-                  </li>
-                  <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 className="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-facebook mr-2 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>Facebook</h6>
-                    <span className="text-secondary">bootdey</span>
-                  </li>
-                </ul>
               </div>
             </div>
-            <div className="col-md-8">
-              <div className="card mb-3">
-                <div className="card-body">
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Full Name</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                      Kenneth Valdez
-                    </div>
-                  </div>
-                  <hr/>
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Email</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                      fip@jukmuh.al
-                    </div>
-                  </div>
-                  <hr/>
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Phone</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                      (239) 816-9029
-                    </div>
-                  </div>
-                  <hr/>
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Mobile</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                      (320) 380-4539
-                    </div>
-                  </div>
-                  <hr/>
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Address</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                      Bay Area, San Francisco, CA
-                    </div>
-                  </div>
-                  <hr/>
-                  <div className="row">
-                    <div className="col-sm-12">
-                      <a className="btn btn-info " target="__blank" href="https://www.bootdey.com/snippets/view/profile-edit-data-and-skills">Edit</a>
-                    </div>
-                  </div>
+
+            <div className={infoStyle.card}>
+              <div className="row">
+                <div className="col-sm-3">
+                  <h6 className="mb-0">Username</h6>
+                </div>
+                <div className="col-sm-9 text-secondary">
+                  {myuser.username ? myuser.username : "none"}{" "}
                 </div>
               </div>
-
-              <div className="row gutters-sm">
-                <div className="col-sm-6 mb-3">
-                  <div className="card h-100">
-                    <div className="card-body">
-                      <h6 className="d-flex align-items-center mb-3"><i className="material-icons text-info mr-2">assignment</i>Project Status</h6>
-                      <small>Web Design</small>
-                      <div className="progress mb-3" //style="height: 5px"
-                      >
-                        <div className="progress-bar bg-primary" role="progressbar" 
-                       // style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"
-                        ></div>
-                      </div>
-                      <small>Website Markup</small>
-                      <div className="progress mb-3" //style="height: 5px"
-                      >
-                        <div className="progress-bar bg-primary" role="progressbar" //style="width: 72%" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"
-                        ></div>
-                      </div>
-                      <small>One Page</small>
-                      <div className="progress mb-3" //style="height: 5px"
-                      >
-                        <div className="progress-bar bg-primary" role="progressbar" 
-                        //style="width: 89%" aria-valuenow="89" aria-valuemin="0" aria-valuemax="100"
-                        ></div>
-                      </div>
-                      <small>Mobile Template</small>
-                      <div className="progress mb-3" //style="height: 5px"
-                      >
-                        <div className="progress-bar bg-primary" role="progressbar" //style="width: 55%" 
-                        aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
-                      </div>
-                      <small>Backend API</small>
-                      <div className="progress mb-3" //style="height: 5px"
-                      >
-                        <div className="progress-bar bg-primary" role="progressbar" //style="width: 66%" 
-                        aria-valuenow="66" aria-valuemin="0" aria-valuemax="100"></div>
-                      </div>
-                    </div>
-                  </div>
+              <hr />
+              <div className="row">
+                <div className="col-sm-3">
+                  <h6 className="mb-0">Email</h6>
                 </div>
-                <div className="col-sm-6 mb-3">
-                  <div className="card h-100">
-                    <div className="card-body">
-                      <h6 className="d-flex align-items-center mb-3"><i className="material-icons text-info mr-2">assignment</i>Project Status</h6>
-                      <small>Web Design</small>
-                      
-                       
-                    </div>
-                  </div>
+                <div className="col-sm-9 text-secondary">
+                  {myuser.email ? myuser.email : "none"}
                 </div>
               </div>
+              <hr />
+              <div className="row">
+                <div className="col-sm-3">
+                  <h6 className="mb-0">DNI</h6>
+                </div>
+                <div className="col-sm-9 text-secondary">
+                  {myuser.dni ? myuser.dni : "none"}
+                </div>
+              </div>
+              <hr />
+              <div className="row">
+                <div className="col-sm-3">
+                  <h6 className="mb-0">Phone</h6>
+                </div>
+                <div className="col-sm-9 text-secondary">
+                  {myuser.tel ? myuser.tel : "none"}
+                </div>
+              </div>
+              <hr />
 
-
+              <div className="row">
+                <div className="col-sm-3">
+                  <h6 className="mb-0">Address</h6>
+                </div>
+                <div className="col-sm-9 text-secondary">
+                  {myuser.address ? myuser.address : "none"}
+                </div>
+              </div>
+              
 
             </div>
           </div>
+        ) : (
+          <img src={loadingSvg} alt="profile" />
+        )}
 
-        </div>
-    </div>
-        </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-        <div>
-    
-
-          {/* <div className="container my-5">
-          <h4>Turns</h4>
-            <div className="shadow-4 rounded-5 overflow-hidden">
-              <table className="table align-middle mb-0 bg-white">
-                <thead className="bg-light">
-                  <tr>
-                    <th>Service</th>
-                    <th>Pet</th>
-                    <th>Vet</th>
-                    <th>Date</th>
-                    <th>Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    myturns?.length ? (
+        <Box>
+          {myturns?.length ? (
+            <div>
+              <div className="container my-5">
+                <h4>Turns</h4>
+                <div className="shadow-4 rounded-5 overflow-hidden">
+                  <table className="table align-middle mb-0 bg-white">
+                    <thead className="bg-light">
                       <tr>
-                      <td>
-                        <div className="d-flex align-items-center">
-                          <img
-                            src={petPlaceholder}
-                            alt=""
-                            
-                            className={"rounded-circle " + infoStyle.tableImg}
-                          />
-                          <div className="ms-3">
-                            <p className="fw-bold mb-1">John Doe</p>
-                            <p className="text-muted mb-0">john.doe@gmail.com</p>
+                      <th>Id</th>
+                        <th>Pet</th>
+                        <th>Vet</th>
+                        <th>Date</th>
+                        <th>Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <div className="d-flex align-items-center">
+                            <img
+                              src={petPlaceholder}
+                              alt="petImg"
+                              className={"rounded-circle " + infoStyle.tableImg}
+                            />
+                            <div className="ms-3">
+                              <p className="fw-bold mb-1">John Doe</p>
+                            </div>
                           </div>
-                        </div>
-                      </td>
-                      <td>
-                        <p className="fw-bold mb-1">Software engineer</p>
-                        <p className="text-muted mb-0">IT department</p>
-                      </td>
-                      <td>
-                        <span className="badge badge-success rounded-pill">
-                          Active
-                        </span>
-                      </td>
-                      <td>Senior</td>
-                      <td>
-                        <button
-                          type="button"
-                          className="btn btn-link btn-sm btn-rounded"
-                        >
-                          Edit
-                        </button>
-                      </td>
-                    </tr>
-                    )
+                        </td>
 
-                    : (
-                      <tr>
-                      <td>
-                        No turns
-                      </td>
-                    </tr>
-                    )
+                        <td>
+                          <p>Especie</p>
+                        </td>
 
-                  }
+                        <td>
+                          <p>detalle</p>
+                        </td>
 
+                        <td>
+                          <button
+                            type="button"
+                            className="btn btn-sm btn-rounded"
+                          >
+                            Edit
+                          </button>
 
-                </tbody>
-              </table>
+                          <button
+                            type="button"
+                            className="btn btn-sm btn-rounded btn-danger"
+                          >
+                            Remove
+                          </button>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
             </div>
-          </div> */}
-        </div>
+          ) : (
+            <div>
+              <div className="container my-5">
+                <h4>Turns</h4>
+                <div className="shadow-4 rounded-5 overflow-hidden">
+                  <table className="table align-middle mb-0 bg-white">
+                    <thead className="bg-light">
+                      <tr>
+                        <th>Id</th>
+                        <th>Pet</th>
+                        <th>Vet</th>
+                        <th>Date</th>
+                        <th>Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <p>0</p>
+                        </td>
+                        <td>
+                          <div className="d-flex align-items-center">
+                            <img
+                              src={petPlaceholder}
+                              alt="petImg"
+                              className={"rounded-circle " + infoStyle.tableImg}
+                            />
+                            <div className="ms-3">
+                              <p className="fw-bold mb-1">No turns</p>
+                            </div>
+                          </div>
+                        </td>
+
+                        <td>
+                          <p></p>
+                        </td>
+
+                        <td>
+                          <p></p>
+                        </td>
+
+                        <td>
+                          <p></p>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          )}
+
+        </Box>
       </Box>
     </Box>
   );
